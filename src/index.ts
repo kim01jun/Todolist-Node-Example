9f871984b00cb73b96211b719562ca03328be23d
WonJunKim <kimjnsjwj@gmail.com>
2019-05-02 22:59:07 +0900

    Modify connect option at index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import api from './routes';
 import config from './util/config';
 
-mongoose.connect(config.MONGODB_URI as string, { useNewUrlParser: true })
+mongoose.connect(config.MONGODB_URI as string, { useNewUrlParser: true, useFindAndModify: true })
   .then(() => console.log('Connected to DB'));
 
 const app = express();
